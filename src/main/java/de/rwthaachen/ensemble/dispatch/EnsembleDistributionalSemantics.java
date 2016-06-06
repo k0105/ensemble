5bdb409bf6067a4c6bdf04f8e01c9212f598df86
k0105 <8035baphgithub>
2016-06-06 15:04:13 -0400

    Added dynamic URLs to keyphrase extraction ensemble.

--- src/main/java/de/rwthaachen/ensemble/dispatch/EnsembleDistributionalSemantics.java
+++ src/main/java/de/rwthaachen/ensemble/dispatch/EnsembleDistributionalSemantics.java
@@ -7,8 +7,8 @@ import de.rwthaachen.ensemble.backend.WebQaSystem;
  */
 public class EnsembleDistributionalSemantics extends Ensemble {
     public void init() {
-        ensembleMembers.add(new WebQaSystem("RAKE", 1.6d, "http://localhost:/jbt/similarTerms?term=&backend="));
-        ensembleMembers.add(new WebQaSystem("RAKE", 1.6d, "http://localhost:4123/wordemb/mostsimilar?setPos="));
+        ensembleMembers.add(new WebQaSystem("JBT", 1.6d, "/jbt/similarTerms?term=&backend="));
+        ensembleMembers.add(new WebQaSystem("Gensim", 1.6d, "/wordemb/mostsimilar?setPos="));
         // http://localhost:4123/wordemb/doesntmatch?set=cat%20dog%20horse%20rabbit%20tiger%20bus
         // http://localhost:4123/wordemb/mostsimilar?setPos=king%20woman&setNeg=man
         //
--- src/main/java/de/rwthaachen/ensemble/dispatch/EnsembleKeyphrase.java
+++ src/main/java/de/rwthaachen/ensemble/dispatch/EnsembleKeyphrase.java
@@ -4,6 +4,7 @@ import de.rwthaachen.ensemble.backend.KeyphraseAlchemy;
 import de.rwthaachen.ensemble.backend.KeyphraseRake;
 import de.rwthaachen.ensemble.backend.WebQaSystem;
 import de.rwthaachen.ensemble.backend.YodaQaSystem;
+import de.rwthaachen.ensemble.communication.UrlManager;
 
 /**
  * Created by fp on 6/3/16.
@@ -12,7 +13,9 @@ public class EnsembleKeyphrase extends Ensemble {
     // http://localhost:4124/keyphrase/extract?plaintext=...
     // http://127.0.0.1:9093/ask?text=...
     public void init() {
-        ensembleMembers.add(new KeyphraseRake("RAKE", 1.6d, "http://localhost:4124/keyphrase/extract?plaintext="));
-        ensembleMembers.add(new KeyphraseAlchemy("Alchemy", 1.7d, "http://127.0.0.1:9093/ask?text="));
+        ensembleMembers.add(new KeyphraseRake("RAKE", 1.6d, UrlManager.lookUpUrl(UrlManager.Backends.RAKE.ordinal()) +
+                "/keyphrase/extract?plaintext="));
+        ensembleMembers.add(new KeyphraseAlchemy("Alchemy", 1.7d,
+                UrlManager.lookUpUrl(UrlManager.Backends.BLUEMIX.ordinal()) + "/ask?text="));
     }
 }
