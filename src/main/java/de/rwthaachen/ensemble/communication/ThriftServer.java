8e9fe974533505b0db8073b42c5513c419aa997e
Yunsheng Bai <yba@umich.edu>
2016-06-07 15:50:09 -0400

    Modify Thrift: NOT tested because 'gradlew run' does not work

--- src/main/java/de/rwthaachen/ensemble/communication/ThriftServer.java
+++ src/main/java/de/rwthaachen/ensemble/communication/ThriftServer.java
@@ -2,38 +2,71 @@ package de.rwthaachen.ensemble.communication;
 
 import de.rwthaachen.ensemble.orchestration.Controller;
 import de.rwthaachen.ensemble.orchestration.Pipeline;
-import org.apache.thrift.server.TServer;
-import org.apache.thrift.server.TSimpleServer;
-import org.apache.thrift.transport.TServerSocket;
-import org.apache.thrift.transport.TServerTransport;
+
+// Thrift java libraries 
+import org.apache.thrift.server.TNonblockingServer;
+import org.apache.thrift.transport.TFramedTransport;
+import org.apache.thrift.transport.TNonblockingServerSocket;
+import org.apache.thrift.transport.TNonblockingServerTransport;
+import org.apache.thrift.server.TThreadedSelectorServer;
+import org.apache.thrift.transport.TSocket;
+import org.apache.thrift.transport.TTransport;
+import org.apache.thrift.transport.TTransportException;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.apache.thrift.TException;
+import org.apache.thrift.TProcessor;
+// Thrift client-side code for registering w/ sirius
+import org.apache.thrift.protocol.TBinaryProtocol;
+import org.apache.thrift.protocol.TProtocol;
 
 /**
- * Created by fp on 5/13/16.
+ * Starts the ensemble server and listens for requests.
  */
 public class ThriftServer {
-
-    public static ThriftHandler thriftHandler;
-    public static LucidaService.Processor lucidaServiceProcessor;
-
-    public static void setup(Controller controller) {
-        thriftHandler = new ThriftHandler(controller);
-        lucidaServiceProcessor = new LucidaService.Processor(thriftHandler);
-        System.out.println("Created handler and processor.");
-
-        Runnable simple = new Runnable() {
-            public void run() { simple(lucidaServiceProcessor); }
-        };
-        new Thread(simple).start();
-    }
-
-    public static void simple(LucidaService.Processor lucidaServiceProcessor) {
+    private static void connectToCMD() {
+        QueryInput query_input = new QueryInput();
+        query_input.type = "ensemble";
+        query_input.data = new ArrayList<String>();
+        query_input.data.add("localhost");
+        query_input.tags = new ArrayList<String>();
+        query_input.tags.add("9090");
+        QuerySpec spec = new QuerySpec();
+        spec.content = new ArrayList<QueryInput>();
+        spec.content.add(query_input);
+        // Initialize thrift objects.
+        TTransport transport = new TSocket("localhost", 8080);
+        TProtocol protocol = new TBinaryProtocol(new TFramedTransport(transport));
+        LucidaService.Client client = new LucidaService.Client(protocol);
         try {
-            TServerTransport serverTransport = new TServerSocket(9090);
-            TServer server = new TSimpleServer(new TServer.Args(serverTransport).processor(lucidaServiceProcessor));
-            System.out.println("Starting server...");
-            server.serve();
-        } catch (Exception e) {
-            e.printStackTrace();
+            transport.open();
+            System.out.println("Connecting to CMD at port " + 8080);
+            // Register itself to CMD.
+            client.create("", spec);
+            transport.close();
+            System.out.println("Successfully connected to CMD");
+        } catch (TException x) {
+            x.printStackTrace();
         }
+    }
+    
+    /** 
+     * Entry point for ensemble.
+     */
+    public static void setup(Controller controller)
+            throws TTransportException, IOException, InterruptedException { 
+        connectToCMD();
+        TProcessor proc = new LucidaService.AsyncProcessor(
+                new ThriftHandler.AsyncThriftHandler(controller));
+        TNonblockingServerTransport transport = new TNonblockingServerSocket(9090);
+        TThreadedSelectorServer.Args arguments = new TThreadedSelectorServer.Args(transport)
+        .processor(proc)    
+        .protocolFactory(new TBinaryProtocol.Factory())
+        .transportFactory(new TFramedTransport.Factory());
+        final TThreadedSelectorServer server = new TThreadedSelectorServer(arguments);
+        System.out.println("ensemble at port 9090");
+        server.serve();
     }
 }
